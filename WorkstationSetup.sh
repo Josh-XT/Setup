25d187637cebc1eafae4c55de6bd722d68823e3b
Josh XT <josh@devxt.com>
2023-01-30 06:36:04 -0500

    Adding cuda sdk and python 3.8

--- WorkstationSetup.sh
+++ WorkstationSetup.sh
@@ -11,6 +11,14 @@ rm google-chrome-stable_current_amd64.deb
 wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
 sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2_amd64.deb
 rm libssl1.1_1.1.1f-1ubuntu2_amd64.deb
+sudo add-apt-repository ppa:deadsnakes/ppa
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/12.0.0/local_installers/cuda-repo-ubuntu2204-12-0-local_12.0.0-525.60.13-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2204-12-0-local_12.0.0-525.60.13-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2204-12-0-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update
+sudo apt-get -y install cuda
 sudo apt update && sudo apt upgrade -y && sudo snap refresh
 sudo apt remove -y libreoffice*
 sudo apt install -y python3
@@ -24,6 +32,8 @@ sudo apt install -y gnome-boxes
 sudo apt install -y apt-transport-https
 sudo apt install -y code
 sudo apt install -y piper
+sudo apt install software-properties-common
+sudo apt install python3.8
 pip install --upgrade pip
 sudo npm install --global yarn
 sudo snap install powershell --classic
