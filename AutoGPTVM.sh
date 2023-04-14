3965157fbfb4f54e6470a71d88009de6f9fd5ed5
Josh XT <josh@devxt.com>
2023-04-14 13:46:57 -0400

    Added Auto-GPT VM

---
+++ AutoGPTVM.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "alias update='sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo snap refresh'" >> ~/.bashrc
+sudo timedatectl set-timezone America/New_York
+sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
+sudo apt remove docker docker-engine docker.io containerd runc -y
+sudo apt update
+sudo apt install ca-certificates curl gnupg lsb-release -y
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-compose -y
+sudo service docker start
+sudo apt install python3-full python3-pip -y
+git clone https://github.com/Josh-XT/Auto-GPT.git
+cd 'Auto-GPT'
+pip install -r requirements.txt
+cp .env.example .env
+# Create and start redis container for Auto-GPT memory
+REDIS_PORT=$(grep -v '^#' .env | grep REDIS_PORT | cut -d '=' -f2) && docker run -d --name redis -p $REDIS_PORT:$REDIS_PORT --env-file .env redis/redis-stack-server:latest
