f4f5c4c16ddb3168fc19190740f9673b94430580
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2024-03-02 11:29:45 -0500

    Update ServerSetup.sh

--- ServerSetup.sh
+++ ServerSetup.sh
@@ -3,7 +3,9 @@ echo "alias update='sudo apt update && sudo apt upgrade -y && sudo apt autoremov
 sudo timedatectl set-timezone America/New_York
 sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
 sudo apt remove docker docker-engine docker.io containerd runc -y
+sudo add-apt-repository ppa:deadsnakes/ppa 
 sudo apt update
+sudo apt install python3.10 python3-pip -y
 sudo apt install ca-certificates curl gnupg lsb-release -y
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -11,4 +13,3 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io docker-compose -y
 sudo service docker start
-sudo apt install python3-full python3-pip -y
